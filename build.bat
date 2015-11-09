02f93514aa5f5b5c86237f197908cffb0f474bdc
dpull <beingstudio@gmail.com>
2015-11-09 22:58:59 +0800

    自动构建脚本

--- build.bat
+++
@@ -1,7 +0,0 @@
-mklink /j 3rd skynet\3rd
-mklink /j lualib-src skynet\lualib-src
-mklink /j service-src skynet\service-src
-mklink /j skynet-src skynet\skynet-src
-
-Set Path=C:\MinGW\msys\1.0\bin;C:\MinGW\bin
-make -f AutoBuild
