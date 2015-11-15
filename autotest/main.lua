f3d57392064d8c8797f4159a9ae46dc352473f94
dpull <beingstudio@gmail.com>
2015-11-15 22:00:41 +0800

    测试xunit

--- autotest/main.lua
+++ autotest/main.lua
@@ -1,10 +1,34 @@
 local skynet = require "skynet"
+local xunit = require "xunit"
 
 local max_client = 64
 
-local function log_test_result(testcase, sucess, errormsg)
+skynet.start(function()
+	local testsuite_name = "skynet-mingw"
+	local testsuite = xunit(testsuite_name)
+	local sucess, watchdog = pcall(function ()
+		local watchdog = skynet.newservice("watchdog")
+		skynet.call(watchdog, "lua", "start", {
+			port = 8888,
+			maxclient = max_client,
+			nodelay = true,
+		})
+		return watchdog;
+	end)
+	testsuite:add("start watchdog", sucess and watchdog, watchdog)
+
+	local output = "output.xml"
+	testsuite:save(output);
+	pcall(function () 
+		local cmd = string.format("appveyor AddTest %s -Framework xUnit -FileName %s", testsuite_name, output)
+		print(cmd)
+		os.execute(cmd)
+	end)  
+	os.exit()
+end)
+
 --[[
-AddTest options:
+appveyor AddTest options:
  
   -Name             - Required. The name of test.
   -Framework        - Required. The name of testing framework, e.g. NUnit, xUnit, MSTest.
@@ -15,32 +39,4 @@ AddTest options:
   -ErrorStackTrace  - Error stack trace of failed test.
   -StdOut           - Standard console output from the test.
   -StdErr           - Error output from the test.
-
-	local cmd = string.format("appveyor AddTest %s -Outcome %s", testcase, sucess and "Passed" or "Failed")
-	if not sucess and errormsg then
-		cmd = cmd .. " -StdErr " .. errormsg
-	end
-	pcall(function () os.execute(cmd) end)  
 ]]
-
-	local cmd = string.format("Test %s %s", testcase, sucess and "Passed" or "Failed")
-	if not sucess and errormsg then
-		cmd = cmd .. " : " .. errormsg
-	end
-	print(cmd);
-end
-
-skynet.start(function()
-	local sucess, watchdog = pcall(function ()
-		local watchdog = skynet.newservice("watchdog")
-		skynet.call(watchdog, "lua", "start", {
-			port = 8888,
-			maxclient = max_client,
-			nodelay = true,
-		})
-		return watchdog;
-	end)
-
-	log_test_result("start watchdog", sucess and watchdog, watchdog)
-	os.exit()
-end)
---
+++ autotest/xunit.lua
@@ -0,0 +1,48 @@
+local template_head = [[
+<assembly name="C:\Projects\GitHub\appvyr-xunit-tests\xUnit_x86_Clr4\bin\Debug\xUnit_x86_Clr4.dll" run-date="2014-11-28" run-time="14:11:45" configFile="C:\Tools\xUnit\xunit.console.clr4.x86.exe.Config" time="0.130" total="1" passed="0" failed="1" skipped="0" environment="32-bit .NET 4.0.30319.34014" test-framework="xUnit.net 1.9.2.1705">
+	<class time="0.130" name="%s" total="1" passed="0" failed="1" skipped="0">
+]]
+local template_foot = [[
+	</class>
+</assembly>
+]]
+
+local template_failed_item = [=[
+    	<test name="%s" type="%s" result="Fail">
+      		<failure exception-type="Xunit.Sdk.TrueException">
+	        	<message><![CDATA[Assert.True() Failure]]></message>
+    	        <stack-trace>   
+    	        	%s
+    	        </stack-trace>
+	      	</failure>
+    	</test>
+]=]
+
+local template_passed_item = [[
+    	<test name="%s" type="%s" result="Pass" />
+]]
+
+local function add_testcase(self, testcase, is_passed, stacktrace)
+	table.insert(self.testcase, {testcase = testcase, is_passed = is_passed, stacktrace = stacktrace})
+end
+
+local function save(self, filename)
+	local file = io.open(filename,"w")
+	file:write(string.format(template_head, self.testsuite))
+	for k, v in ipairs(self.testcase) do
+		if is_passed then
+			file:write(string.format(template_passed_item, v.testcase, self.testsuite))
+		else
+			file:write(string.format(template_failed_item, v.testcase, self.testsuite, stacktrace))
+		end
+	end
+	file:write(template_foot)
+	file:close()
+end
+
+local function create_testsuite(testsuite)
+	return {testsuite = testsuite, testcase = {}, add = add_testcase, save = save, }
+end
+
+return create_testsuite;
+
