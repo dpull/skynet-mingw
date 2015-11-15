b6735819ed6069fd0c24fa57de998786f504e29e
dpull <beingstudio@gmail.com>
2015-11-15 19:44:12 +0800

    需要支持某个测试引擎的输出

--- autotest/main.lua
+++ autotest/main.lua
@@ -3,12 +3,31 @@ local skynet = require "skynet"
 local max_client = 64
 
 local function log_test_result(testcase, sucess, errormsg)
+--[[
+AddTest options:
+ 
+  -Name             - Required. The name of test.
+  -Framework        - Required. The name of testing framework, e.g. NUnit, xUnit, MSTest.
+  -FileName         - Required. File name containg test.
+  -Outcome          - Test outcome: None, Running, Passed, Failed, Ignored, Skipped, Inconclusive, NotFound, Cancelled, NotRunnable
+  -Duration         - Test duration in milliseconds.
+  -ErrorMessage     - Error message of failed test.
+  -ErrorStackTrace  - Error stack trace of failed test.
+  -StdOut           - Standard console output from the test.
+  -StdErr           - Error output from the test.
+
 	local cmd = string.format("appveyor AddTest %s -Outcome %s", testcase, sucess and "Passed" or "Failed")
 	if errormsg then
 		cmd = cmd .. " -StdErr " .. errormsg
 	end
-	print(cmd)
-	pcall(function () os.execute(cmd) end)
+	pcall(function () os.execute(cmd) end)  
+]]
+
+	local cmd = string.format("Test %s %s", testcase, sucess and "Passed" or "Failed")
+	if errormsg then
+		cmd = cmd .. " : " .. errormsg
+	end
+	print(cmd);
 end
 
 skynet.start(function()
